d4b828f84806ef21179a2259f0241f93fe630134
zwepab16 <zwepab16@htlkaindorf.at>
2018-11-07 10:26:38 +0100

    Überarbeitet

--- 3xhif_PLF1/src/gui/WetterTableModel.java
+++ 3xhif_PLF1/src/gui/WetterTableModel.java
@@ -3,12 +3,10 @@ package gui;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import javax.swing.table.AbstractTableModel;
 
